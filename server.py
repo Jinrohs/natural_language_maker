40394f211a192093e838d7d55327154f93bc773e
sunmoonStern <october.sky.2009@gmail.com>
2016-04-23 23:40:36 +0900

    gridを得る

--- server.py
+++ server.py
@@ -2,10 +2,16 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, request, jsonify
+from datetime import date
+from time import mktime
+import time
 import json
 import random
 import googlemaps
 import urllib2
+import requests
+import calendar
+import datetime
 
 app = Flask(__name__)
 
@@ -28,15 +34,30 @@ satelite_data = {
 }
 
 [POS]=[0]
+[TIME]=[1]
+
+def get_localtime(unixtime, pos):
+    url="https://maps.googleapis.com/maps/api/timezone/json?location=" + str(pos[0]) + "," + str(pos[1]) +"&timestamp=" + unixtime
+    s = requests.Session() 
+    timeinfo = s.get(url).json()
+    result =  int(unixtime) + timeinfo["rawOffset"] + timeinfo["dstOffset"] 
+    
+    return result
+
+def get_picurl(data={}):
+    zoom = 6
+    (row, col) = find_grid(zoom, data)
+    url="http://map1.vis.earthdata.nasa.gov/wmts-geo/MODIS_Terra_CorrectedReflectance_TrueColor/default/2012-07-09/EPSG4326_250m/6/" + str(row) + "/" + str(col) + ".jpg"
+    return url
 
 def get_position(_id):
     #response = urllib2.urlopen('http://python.org/')
     #html = response.read()
 
-    #return [35.7084958, -139.8130165]
+    #return [35.7084958, 139.8130165]
     return [40.714224, -73.961452]
 
-# intension 意図
+# intention 意図
 # 0: エラー
 # 1: 雑談
 # 2: 位置情報について
@@ -48,6 +69,8 @@ def select_intention(_id=0, data={}):
     if data[POS]:
         cands.append(2)
 
+    cands.append(3)
+ 
     res = random.choice(cands)
     return res
 
@@ -74,16 +97,45 @@ def generate_posinfo(data={}):
 
     return comment
 
+def generate_timeinfo(data={}):
+    seed = time.time()
+    r1 = random.SystemRandom(seed) 
+    if (r1 > 0.5):
+    	comment = "{0}時なう".format(data[TIME])
+    else:
+    	if (data[TIME] >= 23 or data[TIME] < 6):
+		comment = "{0}時だよ. まだ仕事してんの?" 
+    	if (data[TIME] >= 6 and data[TIME] < 12):
+		comment = "おはー!"
+    	if (data[TIME] >= 12 and data[TIME] < 16):
+		comment = "こんにちはー!"
+    	if (data[TIME] >= 16 and data[TIME] < 23):
+		comment = "こんばんはー!"  
+    return comment
+
 def select_comment(intention=0, data={}):
     if intention == 1:
         comment = generate_zatudan()
     elif intention == 2:
         comment = generate_posinfo(data=data)
+    elif intention == 3:
+	comment = generate_timeinfo(data=data)
     else:
         return "Error!"
 
     return comment
 
+def find_grid(zoom, data={}):
+    ngrid = 2 ** zoom
+    xmin = -180.0
+    xmax = 180.0
+    ymin = 85.0
+    ymax = -85.0    
+    dx = (xmax - xmin)/float(ngrid)
+    dy = (ymax - ymin)/float(ngrid)
+    nx = int(round((data[POS][0] - xmin)/dx))
+    ny = int(round((data[POS][1] - ymin)/dy))	
+    return (nx, ny)
 
 @app.route('/', methods=['GET'])
 def home():
@@ -91,27 +143,29 @@ def home():
         _id       = request.args['id']
         timestamp = request.args['timestamp']
     except Exception as e:
-        print("Oh...")
+        irint("Oh...")
         return "パラメータを正しく設定してください"
 
     # 状況の取得
     data = {}
     pos = get_position(_id)
+    loctime = get_localtime(timestamp, pos)
+    now = datetime.datetime.utcfromtimestamp(loctime) # Unix time -> UTC の naive オブジェクト
     data[POS] = pos
+    data[TIME] = now.hour
 
     # 意図の決定
     intention = select_intention(_id=_id, data=data)
 
     # コメントの生成
     comment = select_comment(intention=intention, data=data)
-
+        
     # response オブジェクトの生成
     response = {"result":[{"comment":comment}]}
 
     response = jsonify(response)
     response.headers['Access-Control-Allow-Origin'] = "*"
     return response
-
 
 gmaps = googlemaps.Client('AIzaSyBaXrjMRZo2WFyYBAtvamA7ukoW70ttYzY')
 with open("zatudan.text") as fp:
